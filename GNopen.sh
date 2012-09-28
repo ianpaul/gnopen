df027a228ba7f5b1f1a33eefdb19d115088e1bdc
Ian Paul <ian@ianpaul.net>
2012-09-29 00:01:38 +0200

    Issue 2: add desktop file search

--- GNopen.sh
+++ GNopen.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 read -p "Name your app: " app
-$app >/dev/null 2>&1 &   
+
+if
+  type $app &>dev/null; 
+then
+  $app >/dev/null 2>&1 &  
+else 
+  grep -h $app /usr/share/applications/*.desktop | grep -m 1 "^Exec=" | sed 's/^Exec=//' | echo "That didn't work. Here's a suggestion:" `sed 's/%.//'` 
+fi 
